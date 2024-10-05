5a00ea716542554de391ecf844418edae62d2667
Tatia Tsiklauri <tatiatsiklauri@Tatias-MacBook-Air.local>
2024-10-05 17:23:02 +0400

    feat: create magniture calculator

--- src/main.py
+++ src/main.py
@@ -3,3 +3,12 @@ def main():
 
 if __name__ == '__main__':
   main()
+import numpy as np
+import pandas as pd
+from obspy import read
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+import os
+
+
+
